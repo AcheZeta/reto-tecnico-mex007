187effdc27a7d618127b601860f972fcffa93b91
AcheZeta <ache.mariana@gmail.com>
2019-06-06 17:00:07 -0500

    ADD: fecth Json

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 bandList = document.getElementById('band-list');
 
-const bands = ["The Plot in You", "The Devil Wears Prada", "Pierce the Veil", "Norma Jean", "The Bled", "Say Anything", "The Midway State", "We Came as Romans", "Counterparts", "Oh, Sleeper", "A Skylit Drive", "Anywhere But Here", "An Old Dog"]
+fetch('https://retos-tecnicos-backend.lizzie136.now.sh/bands')
+    .then((response) => {
+        return response.json();
+    })
+    .then((bands)=> {
+        console.log(bands);
+    });
 
-console.log(bands)
+// const bands = ["The Plot in You", "The Devil Wears Prada", "Pierce the Veil", "Norma Jean", "The Bled", "Say Anything", "The Midway State", "We Came as Romans", "Counterparts", "Oh, Sleeper", "A Skylit Drive", "Anywhere But Here", "An Old Dog"]
+
+// console.log(bands)
 
 
-const tableSort = (bands) => {
-    `${bands}`
-}
+// const tableSort = (bands) => {
+//     `${bands}`
+// }
