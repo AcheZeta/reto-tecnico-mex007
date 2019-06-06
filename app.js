7454f593d22d56b9cb787d3bbf3bf8a6a02efab1
AcheZeta <ache.mariana@gmail.com>
2019-06-06 17:11:12 -0500

    ADD: Sorting bands

--- app.js
+++ app.js
@@ -6,13 +6,7 @@ fetch('https://retos-tecnicos-backend.lizzie136.now.sh/bands')
     })
     .then((bands)=> {
         console.log(bands);
+        let sortBands = bands.sort()
+        console.log(sortBands)
     });
 
-// const bands = ["The Plot in You", "The Devil Wears Prada", "Pierce the Veil", "Norma Jean", "The Bled", "Say Anything", "The Midway State", "We Came as Romans", "Counterparts", "Oh, Sleeper", "A Skylit Drive", "Anywhere But Here", "An Old Dog"]
-
-// console.log(bands)
-
-
-// const tableSort = (bands) => {
-//     `${bands}`
-// }
