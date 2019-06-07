8e7f02812c9182d1163599ad40aff2f79eb19a23
AcheZeta <ache.mariana@gmail.com>
2019-06-07 16:36:11 -0500

    ADD: Comments

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
-bandList = document.getElementById('band-list');
+//Variable with the id of the table element
+const bandList = document.getElementById('band-list');
 
+//Getting the data from a endpoint
 fetch('https://retos-tecnicos-backend.lizzie136.now.sh/bands')
     .then((response) => {
         return response.json();
@@ -8,11 +10,13 @@ fetch('https://retos-tecnicos-backend.lizzie136.now.sh/bands')
         sortBands(bandName)
     });
 
-
+//Function for sorting and regular expression.
 const sortBands = (bandName) => {
     const orderBands = (bandName) => {
+        //RegEx to find articles in the band's name
         return bandName.replace(/^(a |an |the )/i, '')
     };
+        //Function to sort by name 
     let sortingBands = bandName.sort((a, b) => {
         if (orderBands(a) > orderBands(b)) {
             return 1
@@ -20,6 +24,7 @@ const sortBands = (bandName) => {
             return -1
         }
     })
+    //Create a td in a table for each element
     sortingBands.forEach(bandName => {
         bandList.insertAdjacentHTML('beforeend', `
         <tr>
